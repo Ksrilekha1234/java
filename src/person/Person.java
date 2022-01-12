11fbf4d2db6793f6ef22b08769e1fcdf2057d533
Sri Lekha Kakani <srilekha.kakani@gmail.com>
2022-01-12 17:47:19 +0530

    this is my project

---
+++ src/person/Person.java
@@ -0,0 +1,13 @@
+package person;
+
+public class Person {
+	int id=12;
+	double salary = 2000;
+	String gender = "male";
+	String name = "ravi";
+	public static void main(String[] args) {
+		Person person = new Person();
+		System.out.println(person.id);
+		System.out.println(person.name);
+}
+}
